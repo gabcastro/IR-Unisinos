ab8eb468056642344d30f948c7c443d70a13cc93
Gabriel Castro Fernandes <gabrielcastrofernandes@hotmail.com>
2021-09-06 20:57:20 -0300

    stopwords

--- src/Runtime/RankManager/Rank.cs
+++ src/Runtime/RankManager/Rank.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -16,12 +18,45 @@ namespace InformationRetrieval.Runtime.RankManager
 
         public dynamic Execute(dynamic inputData)
         {
-            throw new System.NotImplementedException();
+            return Generate(inputData);
         }
 
         public RankResponse Generate(RankRequest request)
         {
-            throw new System.NotImplementedException();
+            var stopWordsDocuments = RemoveStopWords(request.OrganizedPdfsContent);
+            Stemming(stopWordsDocuments);
+            return new RankResponse {};
+        }
+
+        private List<List<string>> RemoveStopWords(List<List<string>> documents)
+        {
+            var stopWords = _config.GetSection("Stopwords").Get<string[]>();
+
+            List<List<string>> auxListDocuments = new List<List<string>>();
+            foreach (var ii in documents)
+                auxListDocuments.Add(new List<string>(ii)); // necessary to not copy the reference from the main obj
+            
+            var auxRemoveTokens = new List<string>();
+            int i = 0;
+
+            foreach (var vectorDocuments in documents)
+            {
+                foreach (var token in vectorDocuments)
+                {
+                    if (stopWords.Any(t => t.Equals(token)))
+                    {
+                        auxListDocuments[i].RemoveAll(x => x.Equals(token));
+                    }
+                }
+                i++;
+            }
+
+            return auxListDocuments;
+        }
+
+        private void Stemming(List<List<string>> documents)
+        {
+
         }
     }
 }
